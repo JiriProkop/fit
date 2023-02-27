c8932447bb8edd419afc230fe28eba4fd9bd4dd8
Jiří Prokop <xproko47@stud.fit.vutbr.cz>
2023-02-27 14:43:42 +0100

    ips script final update

--- ips/du3/examples/do.sh
+++ ips/du3/examples/do.sh
@@ -2,26 +2,37 @@
 
 #Author: Ježek
 
-LOGIN="xproko47"
+LOGIN="xlogin00"
 
 mkdir $LOGIN
 cd $LOGIN
 mkdir results
-cd ..
+cd results
+for i in {1..3}
+do
+   mkdir $i
+done
+cd ../..
 
-# 1st 
-cd p1-matmul
-make
-mv *.log.pdf p1-result.pdf
-make clean --silent
-mv p1-result.pdf ../$LOGIN/results
+for i in {1..3}
+do
+   echo "---------------$i. run---------------"
 
-# 2nd
-cd ../p2-bandwidth
-make && make run
-./plot-all-results.sh
-mv *.pdf ../$LOGIN/results
-make clean-all --silent
+	# 1st 
+	cd p1-matmul
+	make
+	mv *.log.pdf p1-result.pdf
+	make --silent clean
+	mv p1-result.pdf ../$LOGIN/results/$i
+
+	# 2nd
+	cd ../p2-bandwidth
+	make && make run
+	./plot-all-results.sh
+	mv *.pdf ../$LOGIN/results/$i
+	make --silent clean-all
+
+done
 
 # get info to doc
 cd ..
