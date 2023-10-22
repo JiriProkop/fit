2f57cd095bc276b9628a3e2d7358671b8c309665
Jiří Prokop <xproko47@stud.fit.vutbr.cz>
2023-10-22 22:40:34 +0200

    ISA req msg decypher

--- isa/proj/tftp-server.c
+++ isa/proj/tftp-server.c
@@ -17,7 +17,7 @@
 #define IP_PROTOCOL 0
 #define MAX_PORT_NUM 65535
 #define TFTP_DEFAULT_SERVER_PORT 69
-#define TFTP_DEFAULT_BLOCK_SIZE 512 // FIXME this is true just for the data blocks, initial request can be longer
+#define TFTP_DEFAULT_DATA_SIZE 512 // FIXME this is true just for the data blocks, initial request can be longer
 
 typedef struct {
     long port;
@@ -33,6 +33,7 @@ bool close_socket = false;
 
     @param failure true for EXIT_FAILURE return code, EXIT_SUCCESS otherwise
 */
+// BUG need to return proper fail codes, defined in codes.h
 void free_and_exit(bool failure) {
     free(args.root_path);
     if (close_socket) {
@@ -101,12 +102,53 @@ void sig_handler(int _) {
     free_and_exit(false);
 }
 
+/*
+    buf is expected to be already allocated to n size
 
-void tftp_read(struct sockaddr_in client_address, char msg[]) {
+    @return returns number of read chars
+*/
+size_t get_nchars_from_file(FILE *fp, size_t n, char *buf, int mode) {
+    static int c = 0;
+    size_t i = 0;
+    // put left over letter from last reading
+    if (mode == octet_mode) {
+        if (c == '\n') {
+            buf[i++] = c;
+        } else if (c == '\r') {
+            buf[i++] = '\0';
+        }
+    }
+    for (; i < n; i++) {
+        c = fgetc(fp);
+        if (mode == octet_mode && (c == '\r' || c == '\n')) {
+            buf[i] == '\r';
+            if (++i == n) {
+                return i;
+            }
+            if (c == '\r') {
+                c = '\0';
+            }
+        }
+        buf[i] = c;
+        if (c == EOF) {
+            return i;
+        }
+    }
+    return i;
+}
+
+void tftp_read(struct sockaddr_in client_address, char msg[], int mode, const char file_name[]) {
+    char buf[TFTP_DEFAULT_DATA_SIZE + 4]; // FIXME this is probably true only without extensions
+
+    FILE *fp = fopen(file_name, "r");
+    if (fp == NULL) {
+        printf("Couldn't read the file! (not present or insufficient privileges) \n");
+        free_and_exit(true);
+    }
     /* TODO always check source port num(has to be the same) An error packet
    should be sent to the source of the incorrect packet, while not
    disturbing the transfer.  This can be done only if the TFTP in fact
-   receives a packet with an incorrect TID.  If the supporting protocols
+   receives a packet with an incorrect TID.  If the supportin"g protocols
    do not allow it, this particular error condition will not arise.     */
 
     /*
@@ -114,17 +156,16 @@ void tftp_read(struct sockaddr_in client_address, char msg[]) {
         ------------------------------------------------
        | Opcode |  Filename  |   0  |    Mode    |   0  |
         ------------------------------------------------
-
-    also, more on modes: https://stackoverflow.com/questions/7101068/tftp-protocol-implementation-and-difference-between-netascii-and-octect
-    but how i understand it: binary(raw) - don't change anything
-                             netascii - before sending, do the bellow thing and also revert it when receiving
-                                        change a new line to 'CR LF' and a single carriage return to 'CR NUL'
     */
+    while (1) {
+        size_t data_size = get_nchars_from_file(fp, TFTP_DEFAULT_DATA_SIZE, buf, mode);
+        // send packet, if data_size < TFTP_DEFAULT_DATA_SIZE, break
+    }
 
     exit(EXIT_SUCCESS);
 }
 
-void tftp_write(struct sockaddr_in client_address, char msg[]) {
+void tftp_write(struct sockaddr_in client_address, char msg[], int mode, const char file_name[]) {
     // if file access is ok: send ACK with block num 0
     // TODO always check source port num(has to be the same)
 
@@ -150,23 +191,41 @@ void main_loop() {
         close_socket = true;
     }
 
-    printf("\nWaiting for stuff to happen...\n");
+    printf("Waiting for stuff to happen...\n");
     int bytesrx;
     // FIXME 1st packet can be bigger than this, so you should check the size
-    char buf[TFTP_DEFAULT_BLOCK_SIZE];
+    char buf[TFTP_DEFAULT_DATA_SIZE];
     struct sockaddr_in client_address;
     socklen_t clientlen = sizeof(client_address);
 
     while (1) {
         // recvfrom blocks until msg is received or error encountered
-        bytesrx = recvfrom(server_socket, buf, TFTP_DEFAULT_BLOCK_SIZE, 0, (struct sockaddr *)&client_address, &clientlen);
+        bytesrx = recvfrom(server_socket, buf, TFTP_DEFAULT_DATA_SIZE, 0, (struct sockaddr *)&client_address, &clientlen);
         if (bytesrx < 0) {
             printf("Recvfrom error! \n");
-            free_and_exit(true);
+            continue;
         }
+        char mode_str[9];
+        char file_name[256];
+        char two_buf[2];
+        if(sscanf(buf, "%c%c%s%s", two_buf[0], two_buf[1], file_name, mode_str) == EOF){
+            printf("Wrong request format in msg! \n");
+            continue;
+        }
+        for (int i = 0; mode_str[i]; ++i)
+            mode_str[i] = tolower(mode_str[i]);
 
-        uint16_t code = opcode_from_chars(buf);
+        uint16_t code = opcode_from_chars(two_buf);
         code = ntohs(code);
+        int mode;
+        if (strcmp(mode_str, "octet") == 0) {
+            mode = octet_mode;
+        } else if(strcmp(mode_str, "netascii") == 0){
+            mode = netascii_mode;
+        } else{
+            printf("Unknown mode! \n");
+            continue;
+        }
 
         pid_t pid = fork();
         if (pid == -1) {
@@ -174,9 +233,9 @@ void main_loop() {
             free_and_exit(true);
         } else if (pid == 0) { // child
             if (code == read_req_opcode) {
-                tftp_read(client_address, buf);
+                tftp_read(client_address, buf, mode, file_name);
             } else if (code == write_req_opcode) {
-                tftp_write(client_address, buf);
+                tftp_write(client_address, buf, mode, file_name);
             } else {
                 printf("Received packet has unknown opcode! \n");
                 exit(EXIT_FAILURE);
